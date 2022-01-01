5de363dbc4b9894b9b154a9e15efe97d653175d9
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-01 20:10:03 +0100

    start MERN-CLOUD-DISK

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div >
+hi
+ </div>
+  );
+}
+
+export default App;
--- src/controllers/books.js
+++
@@ -1,9 +0,0 @@
-import BooksModel from '../models/books.js';
-
-export const getAllBooks = async () => {
-	const books = await BooksModel.find({})
-		.populate("author")
-		.populate("customers")
-		.populate("comments");
-	return books;
-}
